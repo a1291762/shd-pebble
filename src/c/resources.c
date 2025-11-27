885842dadc7a89701e55056347e3ffad14ae9d02
Lincoln Ramsay <a1291762@gmail.com>
2025-11-28 00:14:37 +0800

    emery wants a big battery font, chalk wants a small one

--- src/c/resources.c
+++ src/c/resources.c
@@ -13,12 +13,15 @@ void resources_init() {
     // fonts for bigger screens
     int alarm_clock_font = RESOURCE_ID_FONT_ALARM_CLOCK_50;
     date_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+    // chalk wants a small battery font, emery wants a big one
+    battery_font = fonts_get_system_font(PBL_IF_ROUND_ELSE(FONT_KEY_GOTHIC_18_BOLD,
+                                                           FONT_KEY_GOTHIC_24_BOLD));
 #else
     // fonts for smaller screens
     int alarm_clock_font = RESOURCE_ID_FONT_ALARM_CLOCK_45;
     date_font = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
-#endif
     battery_font = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
+#endif
 
     time_font = fonts_load_custom_font(resource_get_handle(alarm_clock_font));
 }
